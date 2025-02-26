6c6b9d036f154c6c28bab86fd14fead3b9de5a72
zezo1895 <zadel1026@gmail.com>
2025-02-26 16:28:52 +0200

    'new'

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -66,6 +66,23 @@ const Hero = () => {
       revertOnUpdate: true,
     }
   );
+    useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
